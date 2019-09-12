d6846d29c38322787102ea749cc300c405c97ad7
Darius <darius.mtn@gmail.com>
2019-09-12 16:36:20 +0200

    feat(video modal): code improvements and multiple video support

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,204 +1,127 @@
-import React, { Component } from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import {Image,
-        Platform,
-        ScrollView,
-        StyleSheet,
-        AppRegistry,
-        Text,
-        Dimensions,
-        TouchableOpacity,
-        Modal,
-        Alert,
-        TouchableHighlight,
-        View} from 'react-native';
-import { Video } from 'expo-av'
+import React, {Component} from 'react';
+import {ExpoConfigView} from '@expo/samples';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    AppRegistry,
+    Text,
+    Dimensions,
+    TouchableOpacity,
+    Modal,
+    Alert,
+    TouchableHighlight,
+    View
+} from 'react-native';
+import {Video} from 'expo-av'
 
-import { MaterialIcons, Octicons } from '@expo/vector-icons';
+import {MaterialIcons, Octicons} from '@expo/vector-icons';
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 export default class SettingsScreen extends Component {
 
-  state = {
-     mute: false,
-     play: false,
-     playbackObject:[],
-    modalVisible: false,
-  }
+    state = {
 
-  _handleVideoRef = component => {
-      this.setState({playbackObject: component})
-  }
+    };
 
 
-  stopVideo = () => {
-    (this.state.playbackObject).stopAsync();
-    this.setState({mute: false});
-    this.setState({play: false});
-  }
-
-  handleVolume = () => {
-      this.setState({mute: !this.state.mute});
-      if (this.state.mute) {
-        (this.state.playbackObject).setVolumeAsync(1);
-      } else {
-        (this.state.playbackObject).setVolumeAsync(0);
-      }
-  }
-
-  handlePlayAndPause = () => {
-      this.setState({play: !this.state.play})
-      if (this.state.play) {
-        (this.state.playbackObject).pauseAsync();
-      } else {
-        (this.state.playbackObject).playAsync();
-      }
-  }
-    setModalVisible(visible) {
-      this.setState({modalVisible: visible});
-      if (!visible)
-        this.stopVideo();
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.tabBarInfoContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.props.navigation.navigate('VideoModal', {
+                                video: require('../assets/videos/zelda_ghibli.mp4')
+                            });
+                        }}>
+                        <Text style={styles.tabBarInfoText}>Question 1</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tabBarInfoContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.setModalVisible(true);
+                        }}>
+                        <Text style={styles.tabBarInfoText}>Question 2</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tabBarInfoContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.setModalVisible(true);
+                        }}>
+                        <Text style={styles.tabBarInfoText}>Question 3</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tabBarInfoContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.setModalVisible(true);
+                        }}>
+                        <Text style={styles.tabBarInfoText}>Question 4</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
     }
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-     <Modal
-       animationType="slide"
-       transparent={false}
-       visible={this.state.modalVisible}
-       onRequestClose={() => {
-         Alert.alert('Modal has been closed.');
-       }}>
-       <View style={styles.container}>
-          <View>
-               <Video
-                 source={require('../assets/videos/zelda_ghibli.mp4')}
-                 ref={this._handleVideoRef}
-                 //isMuted={this.state.mute}
-                 resizeMode="cover"
-                 //shouldPlay //={this.state.play}
-                 style={{ width: width, height: 300, backgroundColor: 'black' }}
-               />
-               <View style={styles.controlBar}>
-                   <MaterialIcons
-                        name={this.state.mute? "volume-mute" : "volume-up"}
-                        size={45}
-                        color="white"
-                        onPress={this.handleVolume}
-                      />
-                    <MaterialIcons
-                      name={this.state.play ? "pause" : "play-arrow"}
-                      size={45}
-                      color="white"
-                      onPress={this.handlePlayAndPause}
-                    />
-               </View>
-             </View>
-               <View style={styles.tabBarInfoContainer}>
-                 <Text
-                 style={styles.tabBarInfoText}
-                   onPress={() => {
-                     this.setModalVisible(!this.state.modalVisible);
-                   }}>
-                   Close
-                 </Text>
-          </View>
-       </View>
-     </Modal>
-
-                    <View style={styles.tabBarInfoContainer}>
-                       <TouchableOpacity
-                         onPress={() => {
-                           this.setModalVisible(true);
-                         }}>
-                         <Text  style={styles.tabBarInfoText}>Question 1</Text>
-                       </TouchableOpacity>
-                    </View>
-                    <View style={styles.tabBarInfoContainer}>
-                       <TouchableOpacity
-                         onPress={() => {
-                           this.setModalVisible(true);
-                         }}>
-                         <Text  style={styles.tabBarInfoText}>Question 2</Text>
-                       </TouchableOpacity>
-                    </View>
-                    <View style={styles.tabBarInfoContainer}>
-                       <TouchableOpacity
-                         onPress={() => {
-                           this.setModalVisible(true);
-                         }}>
-                         <Text  style={styles.tabBarInfoText}>Question 3</Text>
-                       </TouchableOpacity>
-                    </View>
-                    <View style={styles.tabBarInfoContainer}>
-                       <TouchableOpacity
-                         onPress={() => {
-                           this.setModalVisible(true);
-                         }}>
-                         <Text  style={styles.tabBarInfoText}>Question 4</Text>
-                       </TouchableOpacity>
-                    </View>
-   </View>
-    );
-  }
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'Personnality 1',
+    title: 'Personnality 1',
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-  backgroundVideo: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
     },
-  controlBar: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 45,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+    controlBar: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 45,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
 
-  tabBarInfoContainer: {
-    position: 'relative',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-    marginBottom: 10
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
+    tabBarInfoContainer: {
+        position: 'relative',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {width: 0, height: -3},
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+        marginBottom: 10
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
 });
