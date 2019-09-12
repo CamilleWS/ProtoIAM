8dac265e15cc5c715fb17c7bc170975315c20d65
Darius <darius.mtn@gmail.com>
2019-09-12 17:01:59 +0200

    feat(nav): added IAM color

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {Platform} from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
@@ -8,70 +8,76 @@ import Persona1 from '../screens/Persona1';
 import Persona2 from '../screens/Persona2';
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+    web: {headerMode: 'screen'},
+    default: {},
 });
 
 const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  config
+    {
+        Home: HomeScreen,
+    },
+    config
 );
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? 'ios-rocket'
-          : 'md-rocket'
-      }
-    />
-  ),
+    tabBarLabel: 'Home',
+    tabBarIcon: ({focused, tintColor}) => (
+        <TabBarIcon
+            focused={focused}
+            activeTintColor={tintColor}
+            name={
+                Platform.OS === 'ios'
+                    ? 'ios-rocket'
+                    : 'md-rocket'
+            }
+        />
+    ),
 };
 
 HomeStack.path = '';
 
 const Perso1Stack = createStackNavigator(
-  {
-    Perso1: Persona1,
-  },
-  config
+    {
+        Perso1: Persona1,
+    },
+    config
 );
 
 Perso1Stack.navigationOptions = {
-  tabBarLabel: 'Perso 1',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-woman' : 'md-woman'} />
-  ),
+    tabBarLabel: 'Perso 1',
+    tabBarIcon: ({focused, tintColor}) => (
+        <TabBarIcon focused={focused} activeTintColor={tintColor} name={Platform.OS === 'ios' ? 'ios-woman' : 'md-woman'}/>
+    ),
 };
 
 Perso1Stack.path = '';
 
 const Perso2Stack = createStackNavigator(
-  {
-    Perso2: Persona2,
-  },
-  config
+    {
+        Perso2: Persona2,
+    },
+    config
 );
 
 Perso2Stack.navigationOptions = {
-  tabBarLabel: 'Perso 2',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-man' : 'md-man'} />
-  ),
+    tabBarLabel: 'Perso 2',
+    tabBarIcon: ({focused, tintColor}) => (
+        <TabBarIcon focused={focused} activeTintColor={tintColor} name={Platform.OS === 'ios' ? 'ios-man' : 'md-man'}/>
+    ),
 };
 
 Perso2Stack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
-  HomeStack,
-  Perso1Stack,
-  Perso2Stack,
-});
+        HomeStack,
+        Perso1Stack,
+        Perso2Stack,
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: '#7D5FFF',
+        }
+    });
 
 tabNavigator.path = '';
 
