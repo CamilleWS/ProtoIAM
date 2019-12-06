60c2b8847b4fc8a7abc2460c497349a264007d56
Maiguier <maxime.aiguier@epitech.eu>
2019-12-06 18:49:15 +0100

    feat(home): add all functionnality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet,Text, Image, View,SafeAreaView, Dimensions} from 'react-native';
-
+import { StyleSheet,Text, Image, View,SafeAreaView, Dimensions, TouchableOpacity} from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 import Carousel from 'react-native-snap-carousel';
+import Button from '@material-ui/core/Button';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+var indexImage = -1;
 
 export default class App extends React.Component {
 
@@ -13,47 +15,59 @@ export default class App extends React.Component {
         this.state = {
             carouselItems: [
                 {
-                    path:"../assets/images/egypt.jpg",
-                    title:"test1"
+                    title:"egypt"
                 },
                 {
-                    path:"../assets/images/.jpg",
-                    title:"test2"
+                    title:"vitruve-man"
                 },
                 {
-                    path:"../assets/images/vitruve-man.jpg",
-                    title:"test3"
+                    title:"chemist"
                 }
             ]}
     }
 
+    onPress = () => {
+        alert("click !");
+    };
+
 
     _renderItem({item,index}){
+         indexImage = indexImage === 3 ? 0 : indexImage += 1;
         return (
             <View style={{flexGrow:1,justifyContent:'center'}}>
             <Image
-                source={index === 0 ? require("../assets/images/egypt.jpg") : index === 1 ? require ("../assets/images/vitruve-man.jpg"): require("../assets/images/egypt.jpg")}
+                source={indexImage === 0 ? require("../assets/images/egypt.jpg") : indexImage === 1 ? require ("../assets/images/vitruve-man.jpg"): require("../assets/images/chemistry.jpg")}
                 style={{height:"100%", width:"100%", resizeMode:"cover"}}
             />
-             </View>
+            </View>
         )
     }
 
     render() {
         return (
             <View style={styles.container}>
-
                 <Carousel
                     data={this.state.carouselItems}
                     renderItem={this._renderItem}
-                    enableMomentum={true}
                     sliderWidth={viewportWidth}
                     itemWidth={viewportWidth}
                     slideStyle={{ width: viewportWidth }}
                     inactiveSlideOpacity={1}
                     inactiveSlideScale={1}
+                    loop={true}
+                    autoplay={true}
+                    autoplayDelay={0}
+                    enableMomentum={true}
+                    lockScrollWhileSnapping={false}
+                    autoplayInterval={6000}
                 />
-
+            <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                    <TouchableOpacity activOpactity={0.5} style={styles.startButton}>
+                        <View style={{backgroundColor: "white"}}>
+                            <Text style={styles.startText}>Start experience</Text>
+                        </View>
+                    </TouchableOpacity>
+            </View>
             </View>
         );
     }
@@ -64,6 +78,17 @@ const styles = StyleSheet.create({
         flex: 1,
         height:"100%",
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
+    startButton: {
+        backgroundColor: "white",
+        paddingHorizontal: 30,
+        paddingVertical: 5,
+        borderRadius: 30
+    },
+    startText: {
+        fontSize: 40,
+        fontWeight: '100',
+        color: "black",
+    }
 });
