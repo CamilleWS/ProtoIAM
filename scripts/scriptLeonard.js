d0727c243c815919bc18a33c65bb3dd616a51a7e
Fournier <thomas.fournier@epitech.eu>
2019-12-12 17:58:01 +0100

    add variante reponse video

--- scripts/scriptLeonard.js
+++ scripts/scriptLeonard.js
@@ -9,98 +9,8 @@ export class Answear {
 
 let ret = new Answear()
 
-let questions = [
-    "Qui es-tu ?",
-    "Qui est Mona Lisa ?",
-    "Parle moi de tes plus belles inventions ?",
-    "Parle moi de la Joconde.",
-    "Parle moi de la Cène.",
-    "Parle moi de ta passion pour la médecine ?",
-    "Parle moi de ta relation avec François Premier.",
-    "As tu eu des assistants ?",
-    "Où se trouve la Joconde ?",
-    "Où se trouve la Cène?",
-    "Où es tu enterré ?",
-    "Pillais-tu vraiment des morgues pour étudier des cadavres ?",
-    "Quand es-tu décédé ?"
-]
-
-let answers = [
-    "Je suis Léonard de Vinci et je suis né le 15 avril 1452 sur les hauteurs du village de Vinci en Toscane.",
-    "La théorie la plus répandue est que Mona Lisa serait la représentation de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo.",
-    "Je suis particulièrement fier de mes œuvres 'La Joconde' et 'La Cene'",
-    "La Joconde, ou Portrait de Mona Lisa, est un tableau que j’ai réalisé entre 1503 et 1506 ou entre 1513 et 1516, je ne me souviens plus vraiment.",
-    "La Cène est une peinture murale que j’ai réalisée de 1495 à 1498 pour le réfectoire du couvent Dominicain de Santa Maria delle Grazie à Milan.",
-    "J’ai commencé à m’initier à l'anatomie du corps lors de mon apprentissage avec Andrea del Verrocchio, mon maître. Comme artiste, je suis rapidement devenu un expert de l’anatomie topographique, notamment après mes nombreuses études des muscles, des tendons et d'autres caractéristiques anatomiques visibles",
-    "François premier fut mon dernier mécène. Il reconnut ma valeur et m’invita à résider dans le splendide château du Clos Lucé, où je passa le reste de ma vie.",
-    "J’ai eu plusieurs assistants, mais le plus connu reste Salai. D’ailleurs, certains pensent que Salai est le modèle qui m’a servi à réaliser La Joconde !",
-    "La Joconde se trouve actuellement au Louvre.",
-    "La Cène est exposée à l’Église Santa Maria delle Grazie de Milan.",
-    "Je repose à la Chapelle Saint-Hubert à Amboise.",
-    "Oui, mais bien que cela soit mal vue cela m’a permis d’étudier en profondeur l’anatomie du corps humain afin d’en comprendre les mécanismes.",
-    "Je suis décédé le 2 mai 1519.",
-]
-
-let unknwow_answers = [
-    "Désolé, je n'ai pas compris votre question."
-]
-
-let videoPath = [
-    [
-        require("../assets/videos/leonard_de_vinci/presentation-2.mov"),
-        require("../assets/videos/leonard_de_vinci/presentation-1.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/qui-est-la-joconde.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/oeuvres-plus-de-succes.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/presentation-joconde.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/presentation-la-cene.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/passion-medecine.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/relation-francois1er.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/assistants.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/lieu-expo-joconde.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/lieu-expo-cene.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/lieu-enterrement.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/question-dissection.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/date-deces.mov")
-    ],
-    [
-        require("../assets/videos/leonard_de_vinci/desole_je_sais_pas.mp4"),
-        require("../assets/videos/leonard_de_vinci/je_n_ai_pas_la_reponse.mp4")
-    ]
-
-
-]
-
-let unknow_path = [
-        require("../assets/videos/leonard_de_vinci/desole_je_sais_pas.mp4"),
-        require("../assets/videos/leonard_de_vinci/je_n_ai_pas_la_reponse.mp4")
-    ]
-
 let keyWords = [
-    [["qui", "estu"], ["t'es", "qui"], ["tu", "es", "qui"], ["qui", "etesvous"], ["presente", "toi"], ["presentez", "vous"], ["ou", "ne", "tu"], ["ou", "ne", "vous"], ["quand", "ne", "tu"], ["quand", "ne", "vous"], ["ou", "ne", "vous"], ["qui", "parents"],["quel", "metier"], ["pourquoi", "connu"], ["parle", "de", "toi"]],
+    [["qui", "estu"], ["t'es", "qui"], ["tu", "es", "qui"], ["qui", "etesvous"], ["presente", "toi"], ["presentez", "vous"], ["ou", "ne", "tu"], ["ou", "ne", "vous"], ["quand", "ne", "tu"], ["quand", "ne", "vous"], ["ou", "ne", "vous"], ["qui", "parents"],["quel", "metier"], ["pourquoi", "connu"], ["parle", "de", "toi"], ["te", "connaitre"]],
     [["qui", "etait", "monalisa"], ["qui", "etait", "mona"], ["qui", "etait", "lisa"], ["parle", "de", "lisa"], ["parle", "de", "mona"], ["florentine lisa gherardini"], ["qui", "est", "monalisa"], ["qui", "est", "joconde"], ["qui", "etait", "joconde"]],
     [["œuvres"], ["oeuvres"], ["inventions"], ["creations"], ["fier"], ["astu", "fais"], ["tu", "as", "fais"]],
 
@@ -135,6 +45,117 @@ let keyWords = [
     [["estu", "decede"], ["estu", "mort"], ["quand", "tu", "mort"], ["quand", "vous", "mort"], ["annee", "vous", "mort"], ["annee", "tu", "mort"], ["etesvous", "mort"]]
 ]
 
+let questions = [
+    "Qui es-tu ?",
+    "Qui est Mona Lisa ?",
+    "Parle moi de tes plus belles inventions ?",
+    "Parle moi de la Joconde.",
+    "Parle moi de la Cène.",
+    "Parle moi de ta passion pour la médecine ?",
+    "Parle moi de ta relation avec François Premier.",
+    "As tu eu des assistants ?",
+    "Où se trouve la Joconde ?",
+    "Où se trouve la Cène?",
+    "Où es tu enterré ?",
+    "Pillais-tu vraiment des morgues pour étudier des cadavres ?",
+    "Quand es-tu décédé ?"
+]
+
+let answers = [
+    ["Je suis Léonard de Vinci et je suis né le 15 avril 1452 sur les hauteurs du village de Vinci en Toscane."],
+    ["La théorie la plus répandue est que Mona Lisa serait la représentation de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo.", "D'après certains historiens, Mona Lisa serait la Florentine Lisa Gerhardini"],
+    ["Je suis particulièrement fier de mes œuvres 'La Joconde' et 'La Cene'", "je ne sais pas si je peut les qualifier d'invention, mais mes ouevres les plus réputé sont sans aucun doute La joconde et La cene", "La cene et la joconde, ces peintures sont sublimes ! vous ne trouvez pas ?"],
+    ["La Joconde, ou Portrait de Mona Lisa, est un tableau que j’ai réalisé entre 1503 et 1506 ou entre 1513 et 1516, je ne me souviens plus vraiment.", "Je peins la joconde en 3ans, ce tableau est à l'heure actuel, considéré comme l'une de mes plus belles oeuvres !", "Ah, la joconde... Ce tableau représente 3 durs année de travail, mais ca en valait la peine ! "],
+    ["Aaaah oui la Cène... C'est une peinture murale que j’ai réalisée de 1495 à 1498 pour le réfectoire du couvent Dominicain de Santa Maria delle Grazie à Milan.", "J'ai peins la cene pour le réfectoire du couvent dominicain de Santa Maria delle Grazie à Milan. C'est une peinture mural que j'ai réalisé en seulement 3 ans !", "La cene est l'une de mes plus belles oeuvres, sans nul doute ! Je la réalisa entre 1495 et 1498 pour le réfectoir du couvant dominicain de Santa Maria delle Grazie à Millan !"],
+    ["J’ai commencé à m’initier à l'anatomie du corps lors de mon apprentissage avec Andrea del Verrocchio, mon maître. Comme artiste, je suis rapidement devenu un expert de l’anatomie topographique, notamment après mes nombreuses études des muscles, des tendons et d'autres caractéristiques anatomiques visibles"],
+    ["François premier fut mon dernier mécène. Il reconnut ma valeur et m’invita à résider dans le splendide château du Clos Lucé, où je passa le reste de ma vie."],
+    ["J’ai eu plusieurs assistants, mais le plus connu reste Salai. D’ailleurs, certains pensent que Salai est le modèle qui m’a servi à réaliser La Joconde !"],
+    ["La Joconde se trouve actuellement au Louvre.", "La Joconde est exposé au Louvre", "Tu peux la retrouver au Louvre", "Je crois bien qu'elle se trouve au Louvre maintenant"],
+    ["La Cène est exposée à l’Église Santa Maria delle Grazie de Milan.", "La Cène est exposé à l’Église Santa Maria delle Grazie de Milan", "Tu peux la retrouver à l’Église Santa Maria delle Grazie de Milan", "Je crois bien qu'elle se trouve à l’Église Santa Maria delle Grazie de Milant"],
+    ["Je repose à la Chapelle Saint-Hubert à Amboise.", "Je suis enterré à la Chapelle Saint-Hubert à Amboise.", "Ma tombe se situe à Amboise, au seins de la Chapelle Saint-Hubert", "Tu pourrai peut-être me voir à la Chapelle Saint-Hubert à Amboise, c'est là que je repose!"],
+    ["Oui, mais bien que cela soit mal vue cela m’a permis d’étudier en profondeur l’anatomie du corps humain afin d’en comprendre les mécanismes.", "En effet j'ai utiliser certain cadavres afin d'approfondir mes recherche sur l'anatomie humaine."],
+    ["Je suis décédé le 2 mai 1519."]
+]
+
+let unknwow_answers = [
+    "Désolé, je n'ai pas compris votre question."
+]
+
+let videoPath = [
+    [
+        require("../assets/videos/leonard_de_vinci/presentation-2.mov"),
+        require("../assets/videos/leonard_de_vinci/presentation-1.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/qui-est-la-joconde.mov"),
+        require("../assets/videos/leonard_de_vinci/qui-est-mona-lisa-v2.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/oeuvres-plus-de-succes.mov"),
+        require("../assets/videos/leonard_de_vinci/tes-plus-belles-oeuvres-v2.mov"),
+        require("../assets/videos/leonard_de_vinci/tes-plus-belles-oeuvres-v3.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/presentation-joconde.mov"),
+        require("../assets/videos/leonard_de_vinci/parle-moi-de-la-jocondev2.mov"),
+        require("../assets/videos/leonard_de_vinci/parle-moi-de-la-joconde-v3.mov")
+
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/presentation-la-cene.mov"),
+        require("../assets/videos/leonard_de_vinci/parle-moi-de-la-cene-v2.mov"),
+        require("../assets/videos/leonard_de_vinci/parle-moi-de-la-cene-v3.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/passion-medecine.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/relation-francois1er.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/assistants.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/lieu-expo-joconde.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-se-trouve-joconde-v2.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-se-trouve-joconde-v3.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/lieu-expo-cene.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-se-trouve-cene-v2.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-se-trouve-cene-v3.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/lieu-enterrement.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-est-tu-enterre-v2.mov"),
+        require("../assets/videos/leonard_de_vinci/ou-est-tu-enterre-v3.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/question-dissection.mov"),
+        require("../assets/videos/leonard_de_vinci/as-tu-dissequé-v2.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/date-deces.mov")
+    ],
+    [
+        require("../assets/videos/leonard_de_vinci/desole_je_sais_pas.mp4"),
+        require("../assets/videos/leonard_de_vinci/je_n_ai_pas_la_reponse.mp4")
+    ]
+
+
+]
+
+let unknow_path = [
+        require("../assets/videos/leonard_de_vinci/desole_je_sais_pas.mp4"),
+        require("../assets/videos/leonard_de_vinci/je_n_ai_pas_la_reponse.mp4")
+    ]
+
+
+
+let repeat_triggers = [
+    ["repete"], ["mal", "entendu"], ["pas", "compris"], ["hein"], ["peux", "tu", "repeter"], ["quoi"]
+]
+
 let already_use = [
     [0, 0],
     [0],
@@ -155,14 +176,40 @@ let already_use = [
 
 import * as Random from 'expo-random';
 
+export function checkRepeat(text)
+{
+    for (let j = 0; j < repeat_triggers.length; j++) {
+        let goodKeyWord = 0;
+        for (let k = 0; k < repeat_triggers[j].length; k++) {
+            let regexStr = repeat_triggers[j][k]
+            let regexp = new RegExp(regexStr, "gi")
+            if (text.match(regexp)) {
+                goodKeyWord += 1
+            }
+
+        }
+
+        if (goodKeyWord == repeat_triggers[j].length) {
+            return true
+        }
+    }
+    return false
+}
+
 export function checkLeonardQuestion(text)
 {
     let variante = Math.floor(Math.random() * Math.floor(already_use[already_use.length - 1].length))
     text = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     text = text.replace(/[-_?!. ]/gi, '');
-    ret.video_path = unknow_path[variante];
-    ret.str = unknwow_answers[0]
-    ret.question = text
+
+
+    if (!checkRepeat(text)) {
+        ret.video_path = unknow_path[variante];
+        ret.str = unknwow_answers[0]
+        ret.question = text
+    } else {
+        text = ret.question
+    }
 
     for (let i = 0; i < questions.length; i++) {
         for (let j = 0; j < keyWords[i].length; j++) {
@@ -185,8 +232,9 @@ export function checkLeonardQuestion(text)
                     count += 1
                 }
                 already_use[i][variante] = 1;
+                console.log(variante)
                 ret.video_path = videoPath[i][variante];
-                ret.str = answers[i]
+                ret.str = answers[i][variante]
                 ret.question = questions[i]
                 return (ret.video_path)
             }
